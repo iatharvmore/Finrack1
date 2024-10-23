afeacdee627c846fd34741c71e7ef4167a8f2225
Atharv More <118363280+iatharvmore@users.noreply.github.com>
2024-10-23 12:39:03 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,27 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth/cordova";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
-  };
+  apiKey: "AIzaSyCWdW_dSHIzq86xiBMbn1ODuWcp4qOHoxA",
+  authDomain: "finrack-79464.firebaseapp.com",
+  projectId: "finrack-79464",
+  storageBucket: "finrack-79464.appspot.com",
+  messagingSenderId: "978916891149",
+  appId: "1:978916891149:web:d7cda7e3d00c6018615344",
+  measurementId: "G-2F3NB0P6X7"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
+const db = getFirestore(app);
 
 
 
-export { app, auth };
+export { app, auth, db };
